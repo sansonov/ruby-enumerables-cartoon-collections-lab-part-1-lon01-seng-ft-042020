17918688eb7662268334844448fd1e7564ac1813
Adao Natalino <sansonov@gmail.com>
2020-03-31 14:09:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |string, index|
-    a = #{index}.to_i + 1.
-    puts  a #{string}
+    puts "#{index} *#{string}"
   end
 end
